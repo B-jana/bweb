add308759785c3de757cbab0fa1dc141fc9d6b09
B-jana <136323132+B-jana@users.noreply.github.com>
2025-08-07 16:01:24 +0530

    Update Booking.js

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -19,7 +19,7 @@ const Booking = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:8080/api/booking', {
+            const response = await fetch('https://bwebbackend.onrender.com/api/booking', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
