b102277d0f43bcd0b0d953f5f523ab53cd0afca7
B-jana <136323132+B-jana@users.noreply.github.com>
2025-08-12 18:16:15 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ const Home = () => (
             backgroundSize: 'cover',
             backgroundPosition: 'center',
             height: '100vh',
+            width : '80vw'
              padding: '2rem',  
             color: 'white',
             
