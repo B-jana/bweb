73b39b10259d6793b9c06448ca92a83be6194279
B-jana <136323132+B-jana@users.noreply.github.com>
2025-08-13 12:27:32 +0530

    Update Booking.js

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -84,10 +84,6 @@ const Booking = () => {
 
             if (response.ok) {
                     const savedBooking = await response.json();
-                    // Dispatch event so AdminDashboard knows about new booking
-                    const event = new CustomEvent('new-booking', { detail: savedBooking });
-                    window.dispatchEvent(event);
-
                 
                     Swal.fire({
                         icon: 'success',
