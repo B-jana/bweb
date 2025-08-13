99b88637d42d710fcbb893b6a1b521b4b0211d28
B-jana <136323132+B-jana@users.noreply.github.com>
2025-08-13 12:27:56 +0530

    Update Training.js

--- src/components/Training.js
+++ src/components/Training.js
@@ -72,10 +72,6 @@ const Training = () => {
             if (response.ok) {
                 const savedTraining = await response.json();
 
-                 // Dispatch event so AdminDashboard knows about new training
-                const event = new CustomEvent('new-training', { detail: savedTraining });
-                window.dispatchEvent(event);
-                
                 await Swal.fire({
                     icon: 'success',
                     title: 'Enrollment Confirmed!',
