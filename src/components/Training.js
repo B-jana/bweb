2e36bc6b06fee7f23c2cb1c8df557e24948bdf98
B-jana <136323132+B-jana@users.noreply.github.com>
2025-08-14 11:19:42 +0530

    Update Training.js

--- src/components/Training.js
+++ src/components/Training.js
@@ -61,6 +61,13 @@ const Training = () => {
         });
         if (result.isConfirmed){
 
+            // Add listener for mobile back button
+const handleBack = () => {
+    Swal.close(); // close spinner if user navigates back
+    window.removeEventListener("popstate", handleBack);
+};
+window.addEventListener("popstate", handleBack);
+
              Swal.fire({
             background: "transparent", // no background box
             showConfirmButton: false,
